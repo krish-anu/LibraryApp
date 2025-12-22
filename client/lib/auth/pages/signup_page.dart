9dd08e7a4705abe656016c80c51f9839375b9d40
krish-anu <krishnaanu200302@gmail.com>
2025-12-22 21:20:58 +0530

    feat:sign up page

--- client/lib/auth/pages/signup_page.dart
+++ client/lib/auth/pages/signup_page.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:libraryapp/auth/pages/forgot_password.dart';
+import 'package:libraryapp/core/theme/app_pallete.dart';
 
 class SignupPage extends StatefulWidget {
   const SignupPage({super.key});
@@ -10,6 +13,67 @@ class SignupPage extends StatefulWidget {
 class _SignupPageState extends State<SignupPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(28.0),
+          child: Container(
+            child: Column(
+              children: [
+                Text("Sign Up", style: TextStyle(fontSize: 64)),
+                SizedBox(height: 30),
+                TextFormField(
+                  decoration: InputDecoration(hintText: "Name"),
+                ),
+                const SizedBox(height: 30),
+                TextFormField(
+                  decoration: InputDecoration(hintText: "User name"),
+                ),
+                const SizedBox(height: 30),
+                TextFormField(
+                  decoration: InputDecoration(hintText: "Password"),
+                  obscureText: true,
+                ),
+                const SizedBox(height: 10),
+
+               
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: Text("Sign Up"),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Pallete.btn_background,
+                    ),
+                  ),
+                ),
+                RichText(
+                  text: TextSpan(
+                    text: "Already have an account? ",
+                    style: TextStyle(color: Colors.black),
+                    children: [
+                      TextSpan(
+                        text: "Login",
+                        style: TextStyle(color: Colors.red),
+                        recognizer: TapGestureRecognizer()
+                          ..onTap = () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => const SignupPage(),
+                              ),
+                            );
+                          },
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
