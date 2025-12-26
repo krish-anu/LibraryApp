dcacb7971c9a7f15bf9aae0446b50738078927ed
krish-anu <krishnaanu200302@gmail.com>
2025-12-26 12:56:56 +0530

    style:button text colour added

--- client/lib/core/theme/app_pallete.dart
+++ client/lib/core/theme/app_pallete.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class Pallete {
-  static const btnBackground = Colors.green;
+  static const btnBackground = Color.fromARGB(255, 7, 60, 9);
   static const appBarBackground = Color(0xFF1B5E20);
+  static const btnTextColor = Colors.white;
 }
