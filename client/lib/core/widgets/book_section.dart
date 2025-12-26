3e99f5252871388dee45f4784bfa518e9bcfea1e
krish-anu <krishnaanu200302@gmail.com>
2025-12-26 12:58:34 +0530

    feat:section for showing book cards

---
+++ client/lib/core/widgets/book_section.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'book_card.dart';
+
+class BookSection extends StatelessWidget {
+  final List<Map<String, String>> booksDetail;
+  final String heading;
+
+  const BookSection({super.key, required this.booksDetail,required this.heading});
+
+  @override
+  Widget build(BuildContext context) {
+    final books = booksDetail.take(6).toList(); // 🔑 exactly 6
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        // Section title
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+               Text(
+                heading,
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              TextButton(
+                onPressed: () {
+                  // Navigate to all trending
+                },
+                child: const Text('See all'),
+              ),
+            ],
+          ),
+        ),
+
+        const SizedBox(height: 12),
+
+        // Horizontal list
+        SizedBox(
+          height: 260,
+          child: ListView.separated(
+            scrollDirection: Axis.horizontal,
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            itemCount: books.length,
+            separatorBuilder: (_, __) => const SizedBox(width: 12),
+            itemBuilder: (context, index) {
+              final book = books[index];
+              return BookCard(
+                title: book['title']!,
+                author: book['author']!,
+                onTap: () {
+                  // Go to book details (full screen)
+                },
+              );
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
