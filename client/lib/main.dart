3c06975613478fc85b205aba68780e3c2a6789a0
krish-anu <krishnaanu200302@gmail.com>
2025-12-31 08:20:38 +0530

    dark mode added

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,20 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:libraryapp/core/providers/theme_provider.dart';
+import 'package:libraryapp/core/theme/app_pallete.dart';
 import 'package:libraryapp/core/widgets/BottomNavigator/bottom_bar.dart';
 
 void main() {
   runApp(ProviderScope(child: const MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final isDark = ref.watch(isDarkProvider);
     return MaterialApp(
+      themeMode: isDark ? ThemeMode.dark : ThemeMode.light,
       title: 'Flutter Demo',
-      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
+      theme: ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: Pallete.primaryColor,
+        scaffoldBackgroundColor: Pallete.scaffoldBackground,
+      ),
+
+      // 🌙 Dark theme (Material 3)
+      darkTheme: ThemeData(
+        useMaterial3: true,
+        brightness: Brightness.dark,
+        colorSchemeSeed: Pallete.primaryColor,
+      ),
       home: BottomNav(),
     );
   }
