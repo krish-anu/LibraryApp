a180c3ea29eded89ce84029764a5edb914bfcd69
krish-anu <krishnaanu200302@gmail.com>
2026-01-02 16:32:45 +0530

    passing id to book view

--- client/lib/core/widgets/book_view.dart
+++ client/lib/core/widgets/book_view.dart
@@ -3,144 +3,149 @@ import 'package:libraryapp/core/theme/app_pallete.dart';
 import 'package:libraryapp/data/mock_books.dart';
 
 class BookView extends StatelessWidget {
-  const BookView({super.key});
+  final int id;
+
+  const BookView({super.key,required this.id});
 
   @override
   Widget build(BuildContext context) {
-    final book = mockBooks[0];
+
+    final book = mockBooks[id];
     return Scaffold(
       appBar: AppBar(title: Icon(Icons.arrow_back)),
 
       /// The code snippet you provided is defining the body of a Flutter widget called BookView.
-      body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(top: 48, bottom: 36),
-              child: FractionallySizedBox(
-                widthFactor: 0.5,
-                child: Column(
-                  children: [
-                    ClipRRect(
-                      borderRadius: BorderRadiusGeometry.circular(12),
-                      child: Image(
-                        height: 300,
-                        image: AssetImage(
-                          book.image.replaceFirst('client/', ''),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 48, bottom: 36),
+                child: FractionallySizedBox(
+                  widthFactor: 0.5,
+                  child: Column(
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadiusGeometry.circular(12),
+                        child: Image(
+                          height: 300,
+                          image: AssetImage(
+                            book.image.replaceFirst('client/', ''),
+                          ),
+                          fit: BoxFit.cover,
                         ),
-                        fit: BoxFit.cover,
                       ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 24, bottom: 12),
-                      child: Text(
-                        book.title,
+                      Padding(
+                        padding: const EdgeInsets.only(top: 24, bottom: 12),
+                        child: Text(
+                          book.title,
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w400,
+                            color: Pallete.textPrimary,
+                          ),
+                        ),
+                      ),
+                      Text(
+                        book.author,
                         textAlign: TextAlign.center,
                         style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.w400,
-                          color: Pallete.textPrimary,
+                          fontSize: 14,
+                          fontWeight: FontWeight.w200,
+                          color: Pallete.textSecondary,
                         ),
                       ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 8.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(Icons.star, color: Colors.amber, size: 18),
+                            const SizedBox(width: 4),
+                            Text(
+                              book.rating.toString(),
+                              style: TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.w500,
+                                color: Pallete.textPrimary,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 14,
+                          vertical: 6,
+                        ),
+                        decoration: BoxDecoration(
+                          color: Pallete.textSecondary.withOpacity(
+                            0.15,
+                          ), // background color
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: Text(
+                          book.category,
+                          style: TextStyle(
+                            fontSize: 12,
+                            fontWeight: FontWeight.w500,
+                            color: Pallete.textPrimary,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 16),
+                    ],
+                  ),
+                ),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+        
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20, right: 20),
+                    child: Text(
+                      textAlign: TextAlign.start,
+                      "Description",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w300,
+                        color: Colors.black,
+                      ),
                     ),
-                    Text(
-                      book.author,
-                      textAlign: TextAlign.center,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 8, left: 20, right: 20),
+                    child: Text(
+                      book.description,
+                      textAlign: TextAlign.justify,
                       style: TextStyle(
                         fontSize: 14,
-                        fontWeight: FontWeight.w200,
+                        fontWeight: FontWeight.w300,
                         color: Pallete.textSecondary,
+                        height: 1.5,
                       ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 8.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Icon(Icons.star, color: Colors.amber, size: 18),
-                          const SizedBox(width: 4),
-                          Text(
-                            book.rating.toString(),
-                            style: TextStyle(
-                              fontSize: 14,
-                              fontWeight: FontWeight.w500,
-                              color: Pallete.textPrimary,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    Container(
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 14,
-                        vertical: 6,
-                      ),
-                      decoration: BoxDecoration(
-                        color: Pallete.textSecondary.withOpacity(
-                          0.15,
-                        ), // background color
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                      child: Text(
-                        book.category,
-                        style: TextStyle(
-                          fontSize: 12,
-                          fontWeight: FontWeight.w500,
-                          color: Pallete.textPrimary,
-                        ),
-                      ),
-                    ),
-                    const SizedBox(height: 16),
-                  ],
-                ),
-              ),
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(left: 20, right: 20),
-                  child: Text(
-                    textAlign: TextAlign.start,
-                    "Description",
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.w300,
-                      color: Colors.black,
                     ),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 8, left: 20, right: 20),
-                  child: Text(
-                    book.description,
-                    textAlign: TextAlign.justify,
-                    style: TextStyle(
-                      fontSize: 14,
-                      fontWeight: FontWeight.w300,
-                      color: Pallete.textSecondary,
-                      height: 1.5,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            FilledButton(
-              onPressed: () {},
-              style: FilledButton.styleFrom(
-                backgroundColor: book.copiesOwned > 0
-                    ? Pallete.primaryColor
-                    : Colors.red,
+                ],
               ),
-              child: Text(
-                book.copiesOwned > 0 ? "Borrow" : "Reserve",
-
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.w400),
+              FilledButton(
+                onPressed: () {},
+                style: FilledButton.styleFrom(
+                  backgroundColor: book.copiesOwned > 0
+                      ? Pallete.primaryColor
+                      : Colors.red,
+                ),
+                child: Text(
+                  book.copiesOwned > 0 ? "Borrow" : "Reserve",
+        
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.w400),
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
