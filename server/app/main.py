caaf269c699d97bfd121278a95e71033eba5a3ae
krish-anu <krishnaanu200302@gmail.com>
2026-01-07 23:01:48 +0530

    python main file

---
+++ server/app/main.py
@@ -0,0 +1,35 @@
+from contextlib import asynccontextmanager
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from sqlalchemy import text
+from .database import SessionLocal, engine
+from .models.base import Base
+from .models import book, users
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    Base.metadata.create_all(bind=engine)
+    yield
+
+
+app = FastAPI(title="Library App API", lifespan=lifespan)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.get("/")
+def root():
+    return {"message": "Hello, FastAPI + Supabase is working!"}
+
+
+@app.get("/test-db")
+def test_db(db: Session = Depends(get_db)):
+    result = db.execute(text("SELECT 1")).scalar()
+    return {"result": result}
