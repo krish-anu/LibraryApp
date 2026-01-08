cb569c3de32d8bb00dafae8e496aba62d1a008e9
krish-anu <krishnaanu200302@gmail.com>
2026-01-08 23:24:17 +0530

    data type modified for interactions

--- server/app/models/interactions.py
+++ server/app/models/interactions.py
@@ -1,12 +1,12 @@
 from .base import Base
-from sqlalchemy import Column,Integer,ForeignKey,String,DateTime
-from datetime import datetime,timezone
+from sqlalchemy import Column, Integer, ForeignKey, String, DateTime, TEXT
+from datetime import datetime, timezone
 
 # models/interactions.py
 class Interaction(Base):
     __tablename__ = "interactions"
     id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey("users.id"))
-    book_id = Column(Integer, ForeignKey("books.id"))
+    user_id = Column(TEXT, ForeignKey("users.id"))
+    book_id = Column(TEXT, ForeignKey("books.id"))
     interaction_type = Column(String)  # 'view', 'checkout', 'like'
     created_at = Column(DateTime(timezone=True), default=lambda: datetime.now(timezone.utc))
