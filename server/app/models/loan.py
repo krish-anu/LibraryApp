356fcb12f66ef53fd48874b6a5e343eae44d9201
krish-anu <krishnaanu200302@gmail.com>
2026-01-08 22:46:09 +0530

    table for loan

---
+++ server/app/models/loan.py
@@ -0,0 +1,16 @@
+from .base import Base
+from sqlalchemy import Column, TEXT, Date, ForeignKey
+from sqlalchemy.orm import relationship
+
+
+class Loan(Base):
+    __tablename__ = "loans"
+
+    id = Column(TEXT, primary_key=True)
+    book_id = Column(TEXT, ForeignKey("books.id"))
+    member_id = Column(TEXT, ForeignKey("users.id"))
+    loan_date = Column(Date)
+    returned_date = Column(Date)
+
+    book = relationship("Book")
+    member = relationship("User")
