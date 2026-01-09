0ca32878f8b8e4494cf271139d4a23fbf710c8f2
krish-anu <krishnaanu200302@gmail.com>
2026-01-09 11:40:02 +0530

    change category id instead of name in Book model

--- server/app/models/book.py
+++ server/app/models/book.py
@@ -1,5 +1,7 @@
+
 from .base import Base
-from sqlalchemy import Column, TEXT, NUMERIC
+from sqlalchemy import Column, TEXT, NUMERIC, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class Book(Base):
@@ -8,9 +10,15 @@ class Book(Base):
     id = Column(TEXT, primary_key=True)
     title = Column(TEXT)
     author = Column(TEXT)
-    category = Column(TEXT)
+    category_id = Column(TEXT, ForeignKey("categories.id"))
     description = Column(TEXT)
     rating = Column(NUMERIC)
     publication_year = Column(NUMERIC)
     copies_owned = Column(NUMERIC)
     image = Column(TEXT)
+
+    category_rel = relationship("Category", back_populates="books")
+
+    @property
+    def category(self):
+        return self.category_rel.name if self.category_rel else None
