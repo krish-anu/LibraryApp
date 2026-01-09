a1741c6bf1b0fd5a3fb8a51cafe2755e22f5d518
krish-anu <krishnaanu200302@gmail.com>
2026-01-09 11:39:10 +0530

    import category router

--- server/app/main.py
+++ server/app/main.py
@@ -2,7 +2,7 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from .database import engine
 from .models.base import Base
-from .routers import books, loans, general
+from .routers import books, loans, general,category
 
 
 @asynccontextmanager
@@ -16,3 +16,4 @@ app = FastAPI(title="Library App API", lifespan=lifespan)
 app.include_router(general.router)
 app.include_router(books.router)
 app.include_router(loans.router)
+app.include_router(category.router)
