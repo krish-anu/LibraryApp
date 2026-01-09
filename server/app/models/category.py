d6ed9c9556016e7ac6e952474b587cb94549e0ac
krish-anu <krishnaanu200302@gmail.com>
2026-01-09 11:40:22 +0530

    category model

---
+++ server/app/models/category.py
@@ -0,0 +1,12 @@
+from .base import Base
+from sqlalchemy import Column, TEXT
+from sqlalchemy.orm import relationship
+
+
+class Category(Base):
+    __tablename__ = "categories"
+
+    id = Column(TEXT, primary_key=True)
+    name = Column(TEXT)
+
+    books = relationship("Book", back_populates="category_rel")
