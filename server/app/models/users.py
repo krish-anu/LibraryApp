747844ed66c4b61eacb5d1114e30c2dd8f298f47
krish-anu <krishnaanu200302@gmail.com>
2026-01-10 21:35:50 +0530

    add member_id  in user model

--- server/app/models/users.py
+++ server/app/models/users.py
@@ -7,6 +7,7 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(TEXT, primary_key=True)
+    member_id = Column(TEXT)
     name = Column(VARCHAR(100))
     email = Column(VARCHAR(100))
     password = Column(LargeBinary)
