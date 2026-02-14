3c125c2413fed8aaae75b09eb5af32eaee641930
krish-anu <krishnaanu200302@gmail.com>
2026-02-14 17:10:50 +0530

    refactor: enhance server URL handling and logging in ServerConstant; update AndroidManifest for cleartext traffic

--- client/android/build.gradle.kts
+++ client/android/build.gradle.kts
@@ -1,3 +1,6 @@
+import com.android.build.api.dsl.ApplicationExtension
+import com.android.build.api.dsl.LibraryExtension
+
 allprojects {
     repositories {
         google()
@@ -14,28 +17,26 @@ subprojects {
             force("androidx.core:core-ktx:1.15.0")
         }
     }
-
     // Set compileSdk early for plugins that need it during configuration
-    project.plugins.whenPluginAdded {
-        if (this is com.android.build.gradle.api.AndroidBasePlugin) {
-            project.extensions.findByType(com.android.build.gradle.BaseExtension::class.java)?.apply {
-                compileSdkVersion(35)
-            }
+    project.plugins.withId("com.android.application") {
+        project.extensions.configure<ApplicationExtension> {
+            compileSdk = 35
+        }
+    }
+    project.plugins.withId("com.android.library") {
+        project.extensions.configure<LibraryExtension> {
+            compileSdk = 35
         }
     }
 
     afterEvaluate {
-        if (project.hasProperty("android")) {
-            val androidExtension = project.extensions.findByName("android")
-            if (androidExtension != null) {
-                val android = androidExtension as com.android.build.gradle.BaseExtension
-                if (android.namespace == null) {
-                    when (project.name) {
-                        "flutter_appauth" -> android.namespace = "io.crossingthestreams.flutterappauth"
-                        "url_launcher_android" -> android.namespace = "io.flutter.plugins.urllauncher"
-                    }
-                }
-                android.compileSdkVersion(35)
+        val appExt = project.extensions.findByType(ApplicationExtension::class.java)
+        val libExt = project.extensions.findByType(LibraryExtension::class.java)
+        val android = appExt ?: libExt
+        if (android != null && android.namespace == null) {
+            when (project.name) {
+                "flutter_appauth" -> android.namespace = "io.crossingthestreams.flutterappauth"
+                "url_launcher_android" -> android.namespace = "io.flutter.plugins.urllauncher"
             }
         }
     }
--- client/lib/core/constants/server_constant.dart
+++ client/lib/core/constants/server_constant.dart
@@ -1,9 +1,33 @@
 import 'dart:io';
 
 class ServerConstant {
-  static String get serverURL {
-    if (Platform.isAndroid) return 'http://10.0.2.2:8000';
+  static const String _envServer = String.fromEnvironment(
+    'SERVER_URL',
+    defaultValue: '',
+  );
+  static bool _logged = false;
 
-    return 'http://127.0.0.1:8000';
+  static String get serverURL {
+    if (_envServer.isNotEmpty) {
+      if (!_logged) {
+        // ignore: avoid_print
+        print('ServerConstant: using SERVER_URL override -> $_envServer');
+        _logged = true;
+      }
+      return _envServer;
+    }
+
+    // Default to the host loopback. For physical Android devices using
+    // `adb reverse tcp:8000 tcp:8000`, `http://127.0.0.1:8000` reaches the
+    // host machine. If you're using an Android emulator, pass the emulator
+    // host address explicitly when running the app:
+    // `flutter run --dart-define=SERVER_URL=http://10.0.2.2:8000`
+    final url = 'http://127.0.0.1:8000';
+    if (!_logged) {
+      // ignore: avoid_print
+      print('ServerConstant: resolved serverURL -> $url');
+      _logged = true;
+    }
+    return url;
   }
 }
